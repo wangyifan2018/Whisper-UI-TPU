1ebcb20e539d7c14602574c1486e3f069e5e3350
yifan.wang <372104922@qq.com>
2024-03-29 16:16:05 +1100

    fix install scripts

--- Install.sh
+++ Install.sh
@@ -5,6 +5,6 @@ echo ""
 echo "Requirements installed successfully."
 
 python3 -m dfss --url=open@sophgo.com:sophon-demo/Whisper/model_240327/models.zip
-unzip models.zip -D ./models
+unzip models.zip
 rm models.zip
 echo "Models download successfully."
